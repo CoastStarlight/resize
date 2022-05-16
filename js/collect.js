f765bd326d8d6231f366c0a30e2c7eb382497869
CoastStarlight <70343847+CoastStarlight@users.noreply.github.com>
2022-05-16 12:25:04 -0700

    test

---
+++ js/collect.js
@@ -0,0 +1,3 @@
+ctx.moveTo(0,0);
+ctx.lineTo(Width, Height);
+ctx.stroke();
---
+++ js/variables.js
@@ -0,0 +1,5 @@
+var c = document.getElementById("game")
+var ctx = c.getContext("2d")
+
+var Height = 750
+var Width = 1000
